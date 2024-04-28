a590437f86980db3adfdda84de375fd7b355b852
MidAFTech <contact@kitatusstudios.co.uk>
2024-04-28 16:05:10 +0100

    Removing unnessisairy files

--- Scripts/get_latest_source.ps1
+++
@@ -1,3 +0,0 @@
-Get-ChildItem -Path .\Source\*.md -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-$latestFile = (Get-ChildItem -Path .\Source\*.md -Recurse | Sort-Object LastWriteTime -Descending | Select-Object -First 1).FullName
-Write-Host "##teamcity[setParameter name='env.LATEST_MD_FILE' value='$latestFile']"
